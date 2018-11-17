5d3d8549f0eb2222007346c2929b7fe4bfdb085c
ivancolomer <31484161+ivancolomer@users.noreply.github.com>
2018-11-17 16:29:15 +0100

    upd

--- App/Config.php
+++ App/Config.php
@@ -13,18 +13,5 @@ class Config {
     const DB_USER = 'eps';
     const DB_PASSWORD = 'HackEPS_2018';
 
-    const MAIL_HOST = 'smtp.gmail.com';
-    const MAIL_USERNAME = 'username@gmail.com';
-    const MAIL_PASSWORD = 'password';
-    const MAIL_PORT = 587;
-	
-	const MAIL_AUTH_TYPE = 'XOAUTH2';
-	const MAIL_CLIENT_ID = '';
-	const MAIL_CLIENT_SECRET = '';
-	const MAIL_REFRESH_TOKEN = '';
-
-    const CAPTCHA_SITE_KEY = 'google_captcha_invisible_site_key';
-    const CAPTCHA_SECRET = 'google_captcha_invisible_secret_key';
-
     const SHOW_ERRORS = false;
 }
--- App/Models/External/Register.php
+++ App/Models/External/Register.php
@@ -55,28 +55,4 @@ class Register extends \Core\Model {
 
         return [false, "Unkown error."];
     }
-
-    public static function sendMail($email, $username, $hashID, $hash) {
-        try {
-            $mailer = \App\Models\Mailer::getPHPMailer($email);
-
-            $mailer->Subject = 'Signup | Verification of ' . Config::APP_NAME;
-
-            $mailer->Body     = '<h1>Confirm your email address in ' . Config::APP_NAME . '</h1><br>';
-            $mailer->Body    .= '<h3>Hello ' . $username . '! We just need to check that ' . $email . ' is your e-mail address. As soon as we have confirmed it, you will be able to login to ' . Config::APP_NAME . '.</h3><br>';
-            $mailer->Body    .= '<a href="' . \Core\Utils::parseUrlFromString('verify?type=register&id=' . $hashID . '&hash=' . $hash) . '"><h2>Confirm e-mail address</h2></a><br>';
-            $mailer->Body    .= '<h4>' . Config::APP_NAME . ' Team</h4>';
-            
-            $mailer->AltBody  = 'Confirm your email address in ' . Config::APP_NAME . '\r\n';
-            $mailer->AltBody .= 'Hello ' . $username . '! We just need to check that ' . $email . ' is your e-mail address. As soon as we have confirmed it, you will be able to login to ' . Config::APP_NAME . '.\r\n';
-            $mailer->AltBody .= 'Navigate to this url to confirm:\r\n';
-            $mailer->AltBody .= \Core\Utils::parseUrlFromString('verify?type=register&id=' . $hashID . '&hash=' . $hash) . '\r\n';
-            $mailer->AltBody .= '\r\n\r\n' . Config::APP_NAME . ' Team';
-
-            $mailer->send();
-        }
-        catch (Exception $e) {
-            \Core\Error::saveException($e);
-        }
-    }
 }
